1ae932945043a3b7020c9ca6ea66da0c26181708
paulevsGitch <paulevs@yandex.ru>
2023-08-20 23:30:01 +0300

    Fixed missing stairs color

--- src/main/java/paulevs/mfb/block/MFBStairsBlock.java
+++ src/main/java/paulevs/mfb/block/MFBStairsBlock.java
@@ -1,5 +1,7 @@
 package paulevs.mfb.block;
 
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.minecraft.block.BaseBlock;
 import net.minecraft.level.BlockView;
 import net.modificationstation.stationapi.api.registry.Identifier;
@@ -32,6 +34,22 @@ public class MFBStairsBlock extends TemplateStairs {
 	@Override
 	public int getTextureForSide(int side) {
 		return source.getTextureForSide(wrapSide(side), this.meta);
+	}
+	
+	@Override
+	public int getColorMultiplier(BlockView view, int x, int y, int z) {
+		return source.getColorMultiplier(view, x, y, z);
+	}
+	
+	@Override
+	public int getBaseColor(int meta) {
+		return source.getBaseColor(meta);
+	}
+	
+	@Override
+	@Environment(EnvType.CLIENT)
+	public int getRenderPass() {
+		return source.getRenderPass();
 	}
 	
 	private int wrapSide(int side) {
