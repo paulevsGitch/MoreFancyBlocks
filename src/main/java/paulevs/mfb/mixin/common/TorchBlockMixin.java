e1ddd7e00e3a933784883e24e8a0f14dab365d0b
paulevsGitch <paulevs@yandex.ru>
2023-08-23 06:19:24 +0300

    Update to latest StAPI, replaced Redirect with WrapOperation

--- src/main/java/paulevs/mfb/mixin/common/TorchBlockMixin.java
+++ src/main/java/paulevs/mfb/mixin/common/TorchBlockMixin.java
@@ -1,24 +1,25 @@
 package paulevs.mfb.mixin.common;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.block.BaseBlock;
 import net.minecraft.block.FenceBlock;
 import net.minecraft.block.TorchBlock;
 import net.minecraft.level.Level;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import paulevs.mfb.block.MFBWallBlock;
 
 @Mixin(TorchBlock.class)
 public class TorchBlockMixin {
-	@Redirect(method = "isFloorSupport", at = @At(
-		value = "INVOKE",
-		target = "Lnet/minecraft/level/Level;getBlockId(III)I"
-	))
-	private int mfb_torchSupport(Level level, int x, int y, int z) {
+	@WrapOperation(
+		method = "isFloorSupport",
+		at = @At(value = "INVOKE", target = "Lnet/minecraft/level/Level;getBlockId(III)I")
+	)
+	private int mfb_torchSupport(Level level, int x, int y, int z, Operation<Integer> original) {
 		BaseBlock block = level.getBlockState(x, y, z).getBlock();
 		if (block instanceof MFBWallBlock) return BaseBlock.FENCE.id;
 		if (block instanceof FenceBlock) return BaseBlock.FENCE.id;
-		return block.id;
+		return original.call(level, x, y, z);
 	}
 }
